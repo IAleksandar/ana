d4234d4058d3c47b0045ceacf783e6069b2b8bb7
AnaBendovska <anna95bb@gmail.com>
2022-02-18 11:41:29 +0100

    Add files via upload

---
+++ HASELT/Tasks/Task2/Controllers/TextCalculator.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Task1;
+
+namespace Task2.API
+{
+    [Route("api/textcalculator")]
+    [ApiController]
+    public class TextCalculator : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return Ok();
+        }
+
+        [Route("{text}")]
+        [HttpGet]
+        public IActionResult Result([FromRoute] string text)
+        {
+            Task1.TextCalculator t = new Task1.TextCalculator();
+            string result = t.Add(text);
+
+            return Ok(result);
+        }
+    }
+}
